178a39e13e1daf77f44aaac1f85b2497c969cb9b
Tarik Bozüslü <boz.tarik38@gmail.com>
2024-08-25 10:08:00 +0200

    clean coding

---
+++ include.js
@@ -0,0 +1,20 @@
+//HTML einbeziehen
+async function includeHTML() {
+    let includeElements = document.querySelectorAll('[w3-include-html]');
+    for (let element of includeElements) {
+        const file = element.getAttribute("w3-include-html");
+        try {
+            let resp = await fetch(file);
+            if (resp.ok) {
+                element.innerHTML = await resp.text();
+            } else {
+                element.innerHTML = 'Seite nicht gefunden';
+            }
+        } catch (error) {
+            console.error('Fehler beim Einfügen von HTML:', error);
+            element.innerHTML = 'Inhalt konnte nicht geladen werden';
+        }
+    }
+}
+
+includeHTML();
